e7c003e8d385774a06bbc5b03741f30c83a75e41
Dehu Meng <dehu.meng@centurygame.com>
2022-02-13 14:46:11 +0800

    调试

--- src/main.c
+++ src/main.c
@@ -115,7 +115,7 @@ SBIT(S_MOD, _PCON, 7);
  * 阳极商为二极管电流流入方向，由锁存器U2控制，单片机P2IO口控制a(Q1), b(Q2), c(Q3), d(Q4), e(Q5), f(Q6), g(Q7), dp(Q8)
  * 锁存器OE低电平有效，LE(U2(P06),U1(P07))为高电平时，D->Q为输入状态，LE为低电平时，D端输入不影响Q端输出，此时为锁存状态
  */
-
+// 电机开启前，将U2锁存器控制的数码管关闭
 SBIT(U2_LE, _P0, 6);
 // U1的Q5-Q8为电机输出控制位
 SBIT(U1_LE, _P0, 7);
@@ -170,10 +170,6 @@ SBIT(E_1602, _P3, 4);
  * long double		16Byte		128bit
  */
 
-unsigned char ASC_L = 0x00;
-unsigned char ASC_H = 0x01;
-unsigned char ASC_F = 0xFF;
-
 // 数码管编码表
 // 1-4数码管位置
 unsigned char NUM_POS[] = {0xFE, 0xFD, 0xFB, 0xF7};
@@ -203,17 +199,17 @@ void startTick0() {
 	T0_TL = T_NUMBER_1MS;
 	T0_TH = T_NUMBER_1MS >> 8;
 	// 开启T0定时，T0的TR位置1，这里应该是已经默认TF为0
-	// T0_TF = ASC_L;
-	T0_TR = ASC_H;
+	// T0_TF = 0x00;
+	T0_TR = 0x01;
 }
 bool isFullTick0() {
-	return T0_TF == ASC_H;
+	return T0_TF == 0x01;
 }
 void resetTick0() {
 	// 重新调整初始化状态值
 	T0_TL = T_NUMBER_1MS;
 	T0_TH = T_NUMBER_1MS >> 8;
-	T0_TF = ASC_L;
+	T0_TF = 0x00;
 }
 // T1计时器，使用中断方式1，TL溢出后循环读取TH值作为初始值
 // 串口通信中，TL,TH初始值同波特率计算公式获得
@@ -224,25 +220,26 @@ void startTick1() {
 	T_MOD = 0x20;
 	T1_TL = T_NUMBER_SMS;
 	T1_TH = T_NUMBER_SMS;
-	T1_TR = ASC_H;
+	T1_TR = 0x01;
 }
 bool isFullTick1() {
-	return T1_TF == ASC_H;
+	return T1_TF == 0x01;
 }
 void resetTick1() {
 	T1_TL = T_NUMBER_SMS;
 	T1_TH = T_NUMBER_SMS;
-	T1_TF = ASC_L;
+	T1_TF = 0x00;
 }
 
 // 开启串口通信
 void startSerial() {
-	S_REN = ASC_H;
-	S_M0 = ASC_L;
-	S_M1 = ASC_H;
+	S_REN = 0x01;
+	S_M0 = 0x00;
+	S_M1 = 0x01;
 	// TODO 这个地方注释后就可以正常接收到数据？，不知道什么原因
 	// SMOD为0时，波特率不加倍，为1时波特率加倍
-	// S_MOD = 0;
+	// 测试结果：如果SMOD设置为1，调试工具频率加倍不能正常工作，频率一致可以正常工作，已经跟作者反映了问题
+	S_MOD = 0x01;
 }
 // 发送串口数据
 void sendBuf(unsigned char buf) {
@@ -255,25 +252,25 @@ void reciveBuf() {
 
 // 开启T0中断
 void startTimer0Inter() {
-	INT_EA = ASC_H;
-	T0_ET = ASC_H;
+	INT_EA = 0x01;
+	T0_ET = 0x01;
 }
 // 开启T1中断
 void startTimer1Inter() {
-	INT_EA = ASC_H;
-	T1_ET = ASC_H;
+	INT_EA = 0x01;
+	T1_ET = 0x01;
 }
 // 开启外部中断0
 void startInter0Inter() {
-	INT_EA = ASC_H;
-	INT0_EX = ASC_H;
+	INT_EA = 0x01;
+	INT0_EX = 0x01;
 	// IT位设置INT0中断触发方式
-	INT_IT = ASC_H;// 下降沿触发
+	INT_IT = 0x01;// 下降沿触发
 }
 // 开启串口通信中断
 void startInter4Inter() {
-	INT_EA = ASC_H;
-	S_ES = ASC_H;
+	INT_EA = 0x01;
+	S_ES = 0x01;
 }
 
 // 控制led灯闪烁
@@ -289,24 +286,19 @@ void ledLight(unsigned char h, unsigned char l) {
 }
 // 控制蜂鸣器声音
 void beenMusic() {
-	P00 = ((P00 == ASC_H) ? ASC_L : ASC_H);
-}
-void warnMusic() {
-	P00 = ASC_H;
-	delay(50000);
-	P00 = ASC_L;
+	P00 = ((P00 == 0x01) ? 0x00 : 0x01);
 }
 
 // 数码管显示
 void chooseView(unsigned char index) {
 	P2 = NUM_POS[index];// 选择控制哪个数码管
-	U1_LE = ASC_H;// 开启U2锁存器输入模式
-	U1_LE = ASC_L;// 锁存U2输入
+	U1_LE = 0x01;// 开启U2锁存器输入模式
+	U1_LE = 0x00;// 锁存U2输入
 }
 void writeNum(unsigned char num) {
 	P2 = NUM_DIG[num];// 控制数码管显示内容
-	U2_LE = ASC_H;// 开启U1锁存器输入模式
-	U2_LE = ASC_L;// 锁存U1输入
+	U2_LE = 0x01;// 开启U1锁存器输入模式
+	U2_LE = 0x00;// 锁存U1输入
 }
 
 // 液晶显示器
@@ -350,28 +342,50 @@ void viewData(unsigned char row, unsigned char col, char *data) {
 
 /**
  * 步进电机控制
+ * 电机接U1锁存器：控制信号接P07，P07接上拉电阻，默认为高电平
  * 电机启动频率：转动机电每秒最少需要节拍数，假设电机启动频率为550，则每个节拍持续时间要大于1/550，让定时器T1每2ms中断一次
  */
+
+void startMotor() {
+	// 将数码管所有位置0，关闭数码管
+	P2 = 0x00;
+	U2_LE = 1;
+	U2_LE = 0;
+	// 打开U1锁存器控制步进电机，并保持打开状态
+	U1_LE = 1;
+	// 计算节拍
+    // count = (180 * 4096) / 360;
+}
+void rotateMotor() {
+	// 根据步进电机类型，每2ms输入一次节拍
+	P2 = MOTOR_ANG[index];
+	// 循环电机8个节拍
+	index++;
+	if (index > 8)
+	{
+		index = 0;
+	}
+}
 
 void main()
 {
 	// 测试定时器0
-	startTick0();
+	// startTick0();
 	// 测试T0中断
-	startTimer0Inter();
+	// startTimer0Inter();
 
 	// 测试定时器1
-	// startTick1();
-	// 测试T1中断
+	startTick1();
+	// 测试T1中断，串口通信时关闭此中断，用外部中断4
 	// startTimer1Inter();
 	
 	// 测试外部中断0
 	// startInter0Inter();
 
 	// 测试串口通信
-	// startSerial();
+	startSerial();
 	// 测试串口中断
-	// startInter4Inter();
+	startInter4Inter();
 
 	// 测试液晶显示
 	// writeCmd(0x38); //设置 16x2 显示，5x7 点阵，8 位数据接口
@@ -382,11 +396,7 @@ void main()
 	// viewData(2, 3, "Hello World");
 
 	// 测试电机转动
-	P2 = 0x00;// 初始化步进电机
-	U2_LE = 1;
-	U2_LE = 0;
-	// 计算节拍
-    // count = (180 * 4096) / 360;
+	// startMotor();
 
 	// 主循环
     while (1)
@@ -418,17 +428,17 @@ void main()
 		// 	{
 		// 		// count循环累计
 		// 		count = 0;
-		// 		ledH = ((ledH == ASC_F) ? ASC_L : ASC_F);
-		// 		ledL = ((ledL == ASC_L) ? ASC_F : ASC_L);
+		// 		ledH = ((ledH == 0xFF) ? 0x00 : 0xFF);
+		// 		ledL = ((ledL == 0x00) ? 0xFF : 0x00);
 		// 		ledLight(ledH, ledL);
 		// 		beenMusic();
 		// 	}
 		// }
 
 		// 检测串口信息
-		// if (S_RI == ASC_H)
+		// if (S_RI == 0x01)
 		// {
-		// 	S_RI = ASC_L;
+		// 	S_RI = 0x00;
 		// 	P1 = SBUF;
 		// }
     }
@@ -436,8 +446,8 @@ void main()
 
 // 实现外部中断0中断函数
 void inter0Inter() __interrupt(0) {
-	ledH = ((ledH == ASC_F) ? ASC_L : ASC_F);
-	ledL = ((ledL == ASC_L) ? ASC_F : ASC_L);
+	ledH = ((ledH == 0xFF) ? 0x00 : 0xFF);
+	ledL = ((ledL == 0x00) ? 0xFF : 0x00);
 	ledLight(ledH, ledL);
 	// beenMusic();
 }
@@ -445,18 +455,16 @@ void inter0Inter() __interrupt(0) {
 // 实现串口中断函数，从实验来看，中断函数像是不停在轮询
 void inter4Inter() __interrupt(4) {
 	// 退出中断
-	if (S_RI == ASC_H)
+	if (S_RI == 0x01)
 	{
-		S_RI = ASC_L;
+		S_RI = 0x00;
 		reciveBuf();
-		// warnMusic();
 		// 发送数据
 		sendBuf(SBUF);
-		// warnMusic();
 	}
-	else if (S_TI == ASC_H)
+	else if (S_TI == 0x01)
 	{
-		S_TI = ASC_L;
+		S_TI = 0x00;
 	}
 }
 
@@ -467,14 +475,7 @@ void timer0Inter() __interrupt(1) {
 	if (count > 2)
 	{
 		count = 0;
-		P2 = MOTOR_ANG[index];
-		// U2_LE = 1;
-		// U2_LE = 0;
-		index++;
-		if (index > 8)
-		{
-			index = 0;
-		}
+		// rotateMotor();
 	}
 }
 // 实现T1中断函数
@@ -484,8 +485,8 @@ void timer1Inter() __interrupt(3) {
 	if (count > 5000)
 	{
 		count = 0;
-		ledH = ((ledH == ASC_F) ? ASC_L : ASC_F);
-		ledL = ((ledL == ASC_L) ? ASC_F : ASC_L);
+		ledH = ((ledH == 0xFF) ? 0x00 : 0xFF);
+		ledL = ((ledL == 0x00) ? 0xFF : 0x00);
 		ledLight(ledH, ledL);
 		beenMusic();
 	}
